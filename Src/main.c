b5a3604cd3ff682b3fefe3b3f08c9619f024edb3
Andrzej Spintzyk <ben.86.public@gmail.com>
2017-11-01 18:38:24 +0100

    Add few segments to define

--- Src/main.c
+++ Src/main.c
@@ -208,11 +208,31 @@ static void MX_USART1_UART_Init(void)
 static void MX_GPIO_Init(void)
 {
 
+  GPIO_InitTypeDef GPIO_InitStruct;
+
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, Seg2_Pin|Seg3_Pin|Seg4_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(Seg1_GPIO_Port, Seg1_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : Seg2_Pin Seg3_Pin Seg4_Pin */
+  GPIO_InitStruct.Pin = Seg2_Pin|Seg3_Pin|Seg4_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : Seg1_Pin */
+  GPIO_InitStruct.Pin = Seg1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(Seg1_GPIO_Port, &GPIO_InitStruct);
 
 }
 
