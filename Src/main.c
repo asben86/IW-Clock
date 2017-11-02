4c7237e53787146d2354a8c493e977e460f23ea8
Andrzej Spintzyk <ben.86.public@gmail.com>
2017-11-02 21:23:21 +0100

    Debug fixes

--- Src/main.c
+++ Src/main.c
@@ -5,7 +5,7 @@
   ******************************************************************************
   ** This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether
+  * USER CODE END. Other portions of this file, whether 
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
@@ -143,7 +143,7 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct;
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-    /**Initializes the CPU, AHB and APB busses clocks
+    /**Initializes the CPU, AHB and APB busses clocks 
     */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
@@ -154,11 +154,12 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Initializes the CPU, AHB and APB busses clocks
+    /**Initializes the CPU, AHB and APB busses clocks 
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
@@ -167,11 +168,11 @@ void SystemClock_Config(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure the Systick interrupt time
+    /**Configure the Systick interrupt time 
     */
   HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick
+    /**Configure the Systick 
     */
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
@@ -218,9 +219,9 @@ static void MX_USART1_UART_Init(void)
 
 }
 
-/** Configure pins as
-        * Analog
-        * Input
+/** Configure pins as 
+        * Analog 
+        * Input 
         * Output
         * EVENT_OUT
         * EXTI
@@ -240,11 +241,11 @@ static void MX_GPIO_Init(void)
   HAL_GPIO_WritePin(Seg7_GPIO_Port, Seg7_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, SegB_Pin|SegF_Pin|SegA_Pin|Seg2_Pin
+  HAL_GPIO_WritePin(GPIOA, SegB_Pin|SegF_Pin|SegA_Pin|Seg2_Pin 
                           |Seg3_Pin|Seg4_Pin|Seg6_Pin|Seg8_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, SegH_Pin|SegD_Pin|SegC_Pin|SegE_Pin
+  HAL_GPIO_WritePin(GPIOB, SegH_Pin|SegD_Pin|SegC_Pin|SegE_Pin 
                           |SegG_Pin|Seg5_Pin|Seg1_Pin|Seg9_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin : Seg7_Pin */
@@ -253,17 +254,17 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(Seg7_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : SegB_Pin SegF_Pin SegA_Pin Seg2_Pin
+  /*Configure GPIO pins : SegB_Pin SegF_Pin SegA_Pin Seg2_Pin 
                            Seg3_Pin Seg4_Pin Seg6_Pin Seg8_Pin */
-  GPIO_InitStruct.Pin = SegB_Pin|SegF_Pin|SegA_Pin|Seg2_Pin
+  GPIO_InitStruct.Pin = SegB_Pin|SegF_Pin|SegA_Pin|Seg2_Pin 
                           |Seg3_Pin|Seg4_Pin|Seg6_Pin|Seg8_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : SegH_Pin SegD_Pin SegC_Pin SegE_Pin
+  /*Configure GPIO pins : SegH_Pin SegD_Pin SegC_Pin SegE_Pin 
                            SegG_Pin Seg5_Pin Seg1_Pin Seg9_Pin */
-  GPIO_InitStruct.Pin = SegH_Pin|SegD_Pin|SegC_Pin|SegE_Pin
+  GPIO_InitStruct.Pin = SegH_Pin|SegD_Pin|SegC_Pin|SegE_Pin 
                           |SegG_Pin|Seg5_Pin|Seg1_Pin|Seg9_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
@@ -424,7 +425,7 @@ void _Error_Handler(char * file, int line)
   while(1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */ 
 }
 
 #ifdef USE_FULL_ASSERT
@@ -449,10 +450,10 @@ void assert_failed(uint8_t* file, uint32_t line)
 
 /**
   * @}
-  */
+  */ 
 
 /**
   * @}
-*/
+*/ 
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
